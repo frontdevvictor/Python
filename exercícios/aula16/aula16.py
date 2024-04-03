d5768b13d7958fe540adb1acc4e8b54f64f5eb06
Victor Hugo <frontdevvictor@gmail.com>
2024-04-03 13:32:46 -0300

    Atualização pasta exercícios

---
+++ exercícios/aula16/aula16.py
@@ -0,0 +1,25 @@
+pessoa = ('Victor', 20, 'M', 75.20)
+del(pessoa)
+print(pessoa)
+
+"""a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a
+print(c)
+print(c.index(5, 1))"""
+
+"""lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+
+print(sorted(lanche))
+print(lanche)"""
+
+"""for comida in lanche:
+    print(f'Eu vou comer {comida} ')
+
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+
+print('Comi pra caramba!')"""
